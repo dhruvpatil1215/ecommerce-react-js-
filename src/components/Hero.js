dae9f2b76cf619ebef020d4af04ce7624fd0cbe6
dhruvpatil1215 <148572263+dhruvpatil1215@users.noreply.github.com>
2026-02-09 16:00:47 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,13 +12,13 @@ const Hero = () => {
                 </p>
 
                 <div className="hero-buttons">
-                    <a href="product-details.html"><button className="btn primary">BUY NOW</button></a>
-                    <a href="product-details.html"><button className="btn outline">VIEW DETAILS</button></a>
+                    <a href="/product-details.html"><button className="btn primary">BUY NOW</button></a>
+                    <a href="/product-details.html"><button className="btn outline">VIEW DETAILS</button></a>
                 </div>
             </div>
 
-            <div className="hero-image">
-                <a href="product-details.html">
+            <div className="hero-img">
+                <a href="/product-details.html">
                     <img src="/assets/imgs/furniture/banner/chair1.png" alt="Chair" />
                 </a>
             </div>
